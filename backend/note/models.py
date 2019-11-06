81eb8dfb643a5ee985ebe6d3ff2223a8fb3c3674
rickychhoukdean <chhoukdean184@msn.com>
2019-11-06 04:03:54 -0500

    Change  to

--- backend/note/models.py
+++ backend/note/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 # Create your models here.
 
-class Notes(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=20)
     body = models.TextField()
     date_created = models.DateTimeField(auto_now=True)
     date_editted = models.DateTimeField(auto_now_add=True)
 
+
     def _str_(self):
         return self.title
-    
