993a9f7b5f145070e6c6a56dcf8f9c1d10fbbed4
rickychhoukdean <chhoukdean184@msn.com>
2019-11-06 18:20:42 -0500

    Small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,42 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Login from "./views/Login/Login"
-import { connect} from 'react-redux'
-import Signup from "./views/Signup/Signup"
-import Home from "./views/Home/Home"
-import * as actions from "./store/actions/auth"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./views/Login/Login";
+import { connect } from "react-redux";
+import Signup from "./views/Signup/Signup";
+import Home from "./views/Home/Home";
+import * as actions from "./store/actions/auth";
 
-
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
     <Router>
       <div>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
       </div>
     </Router>
   );
-}
+};
 
-const mapStateToProps = (state: { token: null; }) =>{
+const mapStateToProps = (state: { token: null }) => {
   return {
-    isAuthenticated:state.token !==null
-  }
-}
+    isAuthenticated: state.token !== null
+  };
+};
 
-const mapDispatchToProps = (dispatch: (arg0: any) => void) =>{
-  return{
+const mapDispatchToProps = (dispatch: (arg0: any) => void) => {
+  return {
     onTryAutoSignup: () => dispatch(actions.authCheckState())
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ const app = (
   </Provider>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
