6c7b4066d03cd7eb5043e411b7092d028b2ba339
rickychhoukdean <chhoukdean184@msn.com>
2019-11-06 01:17:52 -0500

    Add basic buttons

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 // import rootReducer from './reducers/index'
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import "./Home.scss";
 const Home: React.FC = () => {
+  return (
+    <>
+      <Link className="user-button" to="/login">
+        <Button variant="outline-dark">Login</Button>
+      </Link>
 
-  return (<>
-
-
-  
-</>
+      <Link className="user-button" to="/signup">
+        <Button variant="outline-dark">Signup</Button>
+      </Link>
+    </>
   );
 };
 
